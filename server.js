09bf1e7aedabda780c14c44fec8cb1d9474b8ac2
shashidhar7682 <shashidhar7682@gmail.com>
2024-02-28 23:47:49 +0530

    commit_full_v0.0.3

--- server.js
+++ server.js
@@ -1,37 +1,34 @@
-const express = require('express');
-const fetch = require('node-fetch');
+const exp = require("express");
 
-const app = express();
-const port = 3000;
+const app = exp();
+
+app.use(exp.json());
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server listening on port ${port}...`));
+
+// Define a function to keep the server awake
+const keepAwake = async () => {
+    try {
+        const fetch = await import('node-fetch');
+        const response = await fetch.default('https://vnr-fmp.onrender.com');
+        if (response.ok) {
+            console.log('Server is awake');
+        } else {
+            console.error('Failed to keep server awake');
+        }
+    } catch (error) {
+        console.error(`Error keeping server awake: ${error.message}`);
+    }
+};
 
 // Define a route to keep the server awake
 app.get('/keep-awake', async (req, res) => {
-  try {
-    const response = await fetch('http://yourwebsite.com');
-    if (response.ok) {
-      res.send('Server is awake');
-    } else {
-      res.status(500).send('Failed to keep server awake');
-    }
-  } catch (error) {
-    res.status(500).send(`Error keeping server awake: ${error.message}`);
-  }
+    keepAwake();
+    res.send('Keeping server awake...');
 });
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-// Function to make HTTP request to keep the server awake
-const keepAwake = async () => {
-  try {
-    await fetch('http://localhost:3000/keep-awake');
-    console.log('Server is kept awake');
-  } catch (error) {
-    console.error('Error keeping server awake:', error.message);
-  }
-};
 
 // Schedule keepAwake function to run every 10 minutes
 setInterval(keepAwake, 10 * 60 * 1000); // 10 minutes * 60 seconds * 1000 milliseconds
